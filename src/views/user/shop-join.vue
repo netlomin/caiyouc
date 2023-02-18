384e126d161e348e7db7498b0630cb5a1c366d06
min <min@192.168.0.168>
2023-02-18 23:10:03 +0800

    111

--- src/views/user/shop-join.vue
+++ src/views/user/shop-join.vue
@@ -197,6 +197,7 @@
           shopId: this.shop.id,
         }).then(vo => {
           this.$notify({ message: '添加彩站成功！', background: '#11FF11' })
+          this.$router.back()
         }).catch(api.catch(e => {
           if (e.code == '606') {
             this.$dialog.alert({ title: '提示', message: '你已添加过该彩站！' })
