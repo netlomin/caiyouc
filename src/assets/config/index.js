ba09e0423e9a8b2c2a4d8b9d407f872ca76ae8db
min <min@192.168.0.168>
2023-07-18 08:00:09 +0800

    url优化

--- src/assets/config/index.js
+++ src/assets/config/index.js
@@ -2,7 +2,6 @@ export default {
   themeColorVal: 'F66',
   themeColor: '#F66',
   colors: { blue: '#66F' },
-  baseUrl: 'https://q5nc9bsh3.neiwangyun.net/caiyou',
   key: '2c4e9851-32ac-40f1-b075-c33fade9acf6',
   cp: {
     SSQ: {
